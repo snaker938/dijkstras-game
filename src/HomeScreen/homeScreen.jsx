b149d340d08ff21a28bd44ec77ce46b17b250027
snaker938 <jmendel123@gmail.com>
2022-02-06 13:11:40 +0000

    Started to add a proper home screen (not title)
    I started to add a simple home screen which will allow the player to choose either campaign, which will take them to the main title screen, or the simple sandbox mode.
    
    It consists of a simple light affect made using CSS.
    
    The bulb does scale up and down well enough, but sometimes the actual light affect does not look smooth at all.
    
    Known bugs:
    Errors thrown when user tries to leave the "arena" when an animation is playing. Need a way to cancel the animation when this happens.
    
    The grid itself in the "arena" does not scale up or down well at all. This needs to be fixed as it is a problem for screens of other dimensions.

--- src/HomeScreen/homeScreen.jsx
+++ src/HomeScreen/homeScreen.jsx
@@ -16,27 +16,35 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <>
-        <div>This is the home screen!</div>
-        <button
-          className="play-sandbox-button"
-          onClick={() => EnterSandbox()} /* Enters the sandbox */
-        >
-          Sandbox
-        </button>
-        <button
-          className="play-campaign-button"
-          onClick={() => EnterCampaign()} /* Enters level selection */
-        >
-          Campaign
-        </button>
-        <button
-          className="play-campaign-button"
-          onClick={() =>
-            this.getCurrentLevelData()
-          } /* views available data about the current level */
-        >
-          View Current Level Data
-        </button>
+        <div class="mainBodyHome">
+          <div class="light">
+            <div class="wire"></div>
+            <div class="bulb">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <button
+            className="play-sandbox-button"
+            onClick={() => EnterSandbox()} /* Enters the sandbox */
+          >
+            Sandbox
+          </button>
+          <button
+            className="play-campaign-button"
+            onClick={() => EnterCampaign()} /* Enters level selection */
+          >
+            Campaign
+          </button>
+          <button
+            className="play-campaign-button"
+            onClick={() =>
+              this.getCurrentLevelData()
+            } /* views available data about the current level */
+          >
+            View Current Level Data
+          </button>
+        </div>
       </>
     );
   }
