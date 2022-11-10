2b427c415bc939eb574bb0c1ee1e211b771a6a6c
snaker938 <jmendel123@gmail.com>
2022-11-10 01:20:59 +0000

    Added credits menu
    Fully added credits menu

--- src/HomeScreen/homeScreen.jsx
+++ src/HomeScreen/homeScreen.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { getAllCurrentLevelData } from '../currentLevelHandling';
 import { EnterSandbox, EnterCampaign } from '../Navigation';
 import './homeScreen.css';
 import testingImagePath from '.././assets/mainbackground.png';
@@ -11,11 +10,7 @@ import {
 export default class HomeScreen extends Component {
   constructor() {
     super();
-    this.state = { rerender: [] };
-  }
-
-  getCurrentLevelData() {
-    console.log(getAllCurrentLevelData());
+    this.state = { rerender: [], showCreditsMenu: true };
   }
 
   // This function sets the inputted username, and checks it, and then takes the user to their desired location
@@ -32,24 +27,102 @@ export default class HomeScreen extends Component {
     }
   }
 
+  toggleCreditsMenu() {
+    this.setState({ showCreditsMenu: !this.state.showCreditsMenu });
+  }
+
+  getCreditsMenu() {
+    return (
+      <>
+        <div
+          onClick={() => {}}
+          style={{
+            // backgroundColor: 'rgb(187, 211, 223)',
+            position: 'absolute',
+            width: '100%',
+            height: '200vh',
+            background: '#1a1717',
+            opacity: '0.5',
+            backdropFilter: 'blur(100px)',
+            zIndex: '99',
+          }}
+        ></div>
+        <div style={{ position: 'absolute', left: '-249px', zIndex: '100' }}>
+          <div className="levelInfoContainer">
+            <p
+              style={{ left: '143px', opacity: '1' }}
+              className="levelNameToRender"
+            >
+              Credits
+            </p>
+          </div>
+          <div className="levelInfoContainer2">
+            <div>
+              <div className="creditsDescriptionContainer">
+                <p className="creditsDescription">
+                  This game was written entirely in ReactJS, HTML and CSS. This
+                  code could not have been written without StackOverflow. I
+                  would also like to credit a few other people below.
+                </p>
+              </div>
+            </div>
+            <div>
+              <div
+                className="creditsContainer"
+                style={{ top: '200px', left: '20px' }}
+              >
+                <span>
+                  <div className="creditsInfoTag">Background Game Art</div>
+                  <p className="creditsText">
+                    vivekart on Fiverr (commissioned)
+                  </p>
+                </span>
+              </div>
+            </div>
+            <div>
+              <div
+                className="creditsContainer"
+                style={{ top: '310px', left: '20px' }}
+              >
+                <span>
+                  <div className="creditsInfoTag">Plane Sprite Art</div>
+                  <span className="creditsText">UnLucky Studio (free)</span>
+                </span>
+              </div>
+            </div>
+            <div>
+              <div
+                className="creditsContainer"
+                style={{ top: '420px', left: '20px' }}
+              >
+                <span>
+                  <div className="creditsInfoTag">Plane Sound Effect</div>
+                  <span className="creditsText">soundjay.com (free)</span>
+                </span>
+              </div>
+            </div>
+
+            <button
+              style={{ right: '12px', top: '558px' }}
+              className="optionsMenuButton"
+              onClick={() => {
+                this.toggleCreditsMenu();
+              }}
+            >
+              Exit
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   // Changes the color of the input box text to the default colour (from red), if the user clicks the box before it changes automatically
   changeColor() {
     if (document.getElementById('usernameInput').style.color === 'red') {
       document.getElementById('usernameInput').value = '';
     }
     document.getElementById('usernameInput').style.color = 'white';
-  }
-
-  renderUselessBar(where) {
-    document.getElementById('mainBar').classList = 'bar';
-    for (let i = 0; i <= 100; i++) {
-      setTimeout(function () {
-        if (i === 100) {
-          if (where === 'campaign') EnterCampaign();
-          else EnterSandbox();
-        }
-      }, i * 23);
-    }
   }
 
   // This function clears all local storage data
@@ -60,6 +133,7 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <>
+        {this.state.showCreditsMenu ? this.getCreditsMenu() : null}
         <div
           style={{
             backgroundColor: 'rgb(187, 211, 223)',
@@ -104,7 +178,12 @@ export default class HomeScreen extends Component {
           Medals
         </button>
 
-        <button className="dividerButton creditsButton" onClick={() => {}}>
+        <button
+          className="dividerButton creditsButton"
+          onClick={() => {
+            this.toggleCreditsMenu();
+          }}
+        >
           Credits
         </button>
       </>
