c76abd4728b119334afa446dd0f19c0444920a50
snaker938 <jmendel123@gmail.com>
2022-11-11 11:28:16 +0000

    Dialogue manager first version
    The dialogue manager now replaces variables, such as {userName}, and now loops back to the start if the dialogue is at the end, designated by a 0 for the speaker

--- src/dialogueManager.js
+++ src/dialogueManager.js
@@ -1,10 +1,16 @@
+import { getCurrentUserName } from './currentUserDataHandling';
+
 const { numLevels } = require('./allLevelData');
 const { currentLevel } = require('./currentLevelHandling');
 
-const agentTwo = 'Jenkins';
+const agentTwo = 'Agent Jenkins';
 const dialogueArry = getAllLevelDialogue();
 
 let currentDialogueLineNumber = 0;
+
+export function setCurrentDialogueLineNumber(num) {
+  currentDialogueLineNumber = num;
+}
 
 function getAllLevelDialogue() {
   let levelDialogues = [];
@@ -22,10 +28,28 @@ function getCurrentLevelAllDialogue() {
   return currentLevelAllDialogue;
 }
 
+function parseCurrentDialogue(thingToParse) {
+  let speaker = thingToParse[0];
+  let dialogue = thingToParse[1];
+
+  if (speaker === 1) speaker = `Agent ${getCurrentUserName()}`;
+  else if (speaker === 2) speaker = agentTwo;
+  else if (speaker === null) speaker = '';
+  else {
+    setCurrentDialogueLineNumber(0);
+    getCurrentLevelDialogue();
+    return;
+  }
+
+  if (dialogue.includes('{userName}'))
+    dialogue = dialogue.replace('{userName}', getCurrentUserName());
+  currentDialogueLineNumber = currentDialogueLineNumber + 1;
+  console.log(speaker, dialogue);
+}
+
 export function getCurrentLevelDialogue() {
   let currentLevelAllDialogue = getCurrentLevelAllDialogue();
-  console.log(currentLevelAllDialogue[currentDialogueLineNumber]);
-  currentDialogueLineNumber = currentDialogueLineNumber + 1;
+  parseCurrentDialogue(currentLevelAllDialogue[currentDialogueLineNumber]);
 
   return;
 }
